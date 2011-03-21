7dff088ea37e9291d018f3a3e5e05755162bc3e2
Stephen Bezek <steve@stevebezek.com>
2011-03-21 09:18:15 -0500

    Solved Euler 31.

--- src/euler.clj
+++ src/euler.clj
@@ -253,3 +253,24 @@
         best-tuple (apply max-key #(:num-primes %) all-results)]
     best-tuple))
 
+;Problem 31
+;In England the currency is made up of pound, £, and pence, p, and there are eight coins in general circulation:
+;
+;    1p, 2p, 5p, 10p, 20p, 50p, £1 (100p) and £2 (200p).
+;
+;It is possible to make £2 in the following way:
+;
+;    1?£1 + 1?50p + 2?20p + 1?5p + 1?2p + 3?1p
+;
+;How many different ways can £2 be made using any number of coins?
+(defn euler31-core [amount-left coins]
+  (cond
+    (or (< amount-left 0) (empty? coins)) 0
+    (= amount-left 0) 1
+    :else
+    (+ (euler31a-core (- amount-left (first coins)) coins)
+      (euler31a-core amount-left (rest coins)))))
+
+(defn euler31 []
+  (println "200")
+  (euler31a-core 200 [200 100 50 20 10 5 2 1]))
